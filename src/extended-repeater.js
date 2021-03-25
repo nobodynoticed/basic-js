913252363746a8f0cc5a25faebfb87baa385a766
nobodynoticed <eugenejukov23@gmail.com>
2021-03-25 14:16:24 +0300

    implement what-season function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,14 +1,14 @@
 const CustomError = require('../extensions/custom-error');
 
 module.exports = function repeater(str, options) {
-  let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options;
-  if (typeof str !== 'string') {
-    str = String(str);
-  }
-  if (typeof addition !== 'string') {
-    addition = String(addition);
-  }
-  const repeatedAdd = addition + `${additionSeparator}${addition}`.repeat(additionRepeatTimes - 1);
-  const result = str + repeatedAdd + `${separator}${str}${repeatedAdd}`.repeat(repeatTimes - 1);
-  return result;
+	let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options;
+	if (typeof str !== 'string') {
+		str = String(str);
+	}
+	if (typeof addition !== 'string') {
+		addition = String(addition);
+	}
+	const repeatedAdd = addition + `${additionSeparator}${addition}`.repeat(additionRepeatTimes - 1);
+	const result = str + repeatedAdd + `${separator}${str}${repeatedAdd}`.repeat(repeatTimes - 1);
+	return result;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,16 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+	if (!date) return 'Unable to determine the time of year!';
+	try {
+		const month = date.toJSON().split('-')[1];
+
+		if (Math.floor(month / 3) === 0) return 'winter';
+		if (Math.floor(month / 3) === 1) return 'spring';
+		if (Math.floor(month / 3) === 2) return 'summer';
+		if (Math.floor(month / 3) === 3) return 'autumn';
+		if (Math.floor(month / 3) === 4) return 'winter';
+	} catch {
+		throw 'Error';
+	}
 };
